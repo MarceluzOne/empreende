eb5df801cf1561da4344804e0a1791ee4b9751cf
Marcelo Arruda <arruda16.marcelo@gmail.com>
2026-04-06 01:10:15 -0300

    fix: correção de mensagem

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,6 +7,6 @@
         <h3 class="text-xl md:text-2xl font-semibold mb-4 text-gray-700">
             Bem-vindo, {{ auth()->user()->name }}!
         </h3>
-        <p class="text-gray-600">O sistema está pronto para uso mobile.</p>
+        
     </div>
 @endsection
